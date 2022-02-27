fdd91262c4edb296c2d3aca03d6062a988aafd01
perroudon <patrick.perroudon@gmail.com>
2022-02-27 17:04:13 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent} from 'react';
+  
+const App: FunctionComponent = () => {
+ const name: String = 'les gens';
+    
+ return (
+  <h1>Hello, {name} !</h1>
+ )
+}
+  
+export default App;
